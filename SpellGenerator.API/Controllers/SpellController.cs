e59ad7ec75ee8775102568e29d5b5da1d2459a84
Tyralf <dorian.h-g@hotmail.fr>
2024-08-23 15:27:03 +0200

    Ajoutez des fichiers projet.

---
+++ SpellGenerator.API/Controllers/SpellController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpellGenerator.API.Requests;
+
+namespace SpellGeneratorAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SpellController : ControllerBase
+    {
+        private readonly ILogger<SpellController> _logger;
+
+        public SpellController(ILogger<SpellController> logger)
+        {
+            _logger = logger;
+        }
+
+        // Route: /Spell/GetSpellTest
+        [HttpGet("GetSpellTest")]
+        public string GetSpellTest()
+        {
+            return "SpellTest";
+        }
+
+        // Route: /Spell/GetZbaam
+        [HttpGet("GetZbaam")]
+        public string GetZbaam()
+        {
+            return "Zbaam <3";
+        }
+
+        // Route: /Spell/AddNumbers (HTTP POST)
+        [HttpPost("AddNumbers")]
+        public IActionResult AddNumbers([FromBody] AdditionRequest request)
+        {
+            if (request == null || !ModelState.IsValid)
+            {
+                return BadRequest("Invalid input");
+            }
+
+            var result = request.Number1 + request.Number2;
+            return Ok(new { Sum = result });
+        }
+    }
+}
---
+++ SpellGenerator.API/Requests/AdditionRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace SpellGenerator.API.Requests
+{
+    public class AdditionRequest
+    {
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+    }
+}
