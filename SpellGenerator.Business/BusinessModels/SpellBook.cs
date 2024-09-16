30fd99227e66e1100e8983e4a3753e225ed50c87
Tyralf <dorian.h-g@hotmail.fr>
2024-09-16 10:34:30 +0200

    [Dev] Ajout de User et SpellBook dans BuisnessModels
    [Infra] Ajout d'EntityFrameworkCore au projet Data

---
+++ SpellGenerator.Business/BusinessModels/SpellBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellGenerator.Business.BusinessModels
+{
+    public class SpellBook
+    {
+        public int Id { get; set; }
+        public List<Spell> StoredSpells { get; set; } = new List<Spell>();
+    }
+}
